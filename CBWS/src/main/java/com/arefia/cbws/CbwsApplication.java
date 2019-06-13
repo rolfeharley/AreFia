765ddb585c898058a14e81019d238fac17731729
rolfeharley <rolfe.harley@me.com>
2019-06-13 18:00:19 +0800

    Edit At Gemtek 20190613

--- CBWS/src/main/java/com/arefia/cbws/CbwsApplication.java
+++ CBWS/src/main/java/com/arefia/cbws/CbwsApplication.java
@@ -1,25 +1,14 @@
 package com.arefia.cbws;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.arefia.cbws.service.orderDataHandler;
-import com.arefia.cbws.tools.hmacCodeHandler;
-
 @SpringBootApplication
-public class CbwsApplication {
-	private static final Logger log = LogManager.getLogger(CbwsApplication.class);
-	
-	@Autowired
-	orderDataHandler odh;
-	
-	@Autowired
-	hmacCodeHandler hch;
-	
+public class CbwsApplication {	
 	public static void main(String[] args) {
 		SpringApplication.run(CbwsApplication.class, args);
+		
+		cbwsCheckInPoint cbip = new cbwsCheckInPoint();
+		cbip.testGetOrder();
 	}
 }
--- CBWS/src/main/java/com/arefia/cbws/tools/hmacCodeHandler.java
+++ CBWS/src/main/java/com/arefia/cbws/tools/hmacCodeHandler.java
@@ -8,29 +8,24 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Service;
 
-@Service
 public class hmacCodeHandler {
 	private static final Logger log = LogManager.getLogger(hmacCodeHandler.class);
 	
     public static String hmacEncoder(String secret, String oristr) {
-    	try {
-        	byte[] secsb = secret.getBytes("UTF-8");
-        	byte[] orisb = oristr.getBytes("UTF-8");
-    		Mac hmac = Mac.getInstance("HmacSHA256");
-        	SecretKeySpec hskey = new SecretKeySpec(secsb, "HmacSHA256");
-            hmac.init(hskey);
-            String hencstr = Base64.encodeBase64String(hmac.doFinal(orisb));
-            
-        	return hencstr;
-    	} catch(Exception e) {
-    		log.error(e.getLocalizedMessage(), e);
-        	return null;
-    	}
-    }
-    
-    public String hmacDecoder(String hmacstr) {
-    	
-    	
-    	return "";
+    	return "OK";
+//    	try {
+//    		log.info("ORI:---------------------------------------------\n" + oristr);
+//        	byte[] secsb = secret.getBytes("UTF-8");
+//        	byte[] orisb = oristr.getBytes("UTF-8");
+//    		Mac hmac = Mac.getInstance("HmacSHA256");
+//        	SecretKeySpec hskey = new SecretKeySpec(secsb, "HmacSHA256");
+//            hmac.init(hskey);
+//            String hencstr = Base64.encodeBase64String(hmac.doFinal(orisb));
+//            log.info("RES:---------------------------------------------\n" + hencstr);
+//        	return hencstr;
+//    	} catch(Exception e) {
+//    		log.error(e.getLocalizedMessage(), e);
+//        	return null;
+//    	}
     }
 }
