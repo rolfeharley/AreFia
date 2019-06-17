277803672fed9bfdd77b3ae644944bfef92de07d
rolfeharley <rolfe.harley@me.com>
2019-06-18 00:41:55 +0800

    Swift Starter

---
+++ swiftHello/swiftHello/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swiftHello
+//
+//  Created by Rolfe Han on 2019/6/18.
+//  Copyright © 2019 aRefia. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
